809858eb60cbc340942ddf5db64b01e37619366d
Keith Corona <keithcorona23@gmail.com>
2020-07-26 09:21:45 -0700

    flex rsp

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,8 +2,9 @@ import React from "react"
 import { Link } from "gatsby"
 
 export default props => 
-    <div style={{display: `flex`, flexDirection: `row`, justifyContent: `space-between`, backgroundColor: `#072B5E`, marginTop: `3rem`, padding: `5rem 2rem`, alignItems: `flex-start`}}>
-        <div style={{ display: `flex`, flexDirection: `column`, alignItems: `flex-start`, backgroundColor: `#072B5E`, fontSize: `1.3rem`}}>
+    <div className="footer-flex">
+        
+        <div className="footer-text" style={{ display: `flex`, flexDirection: `column`, alignItems: `flex-start`, backgroundColor: `#072B5E`, fontSize: `1.3rem`}}>
             <Link style={{ color: `#EDE90C`, }} to="/">
                 <p>{props.footerReturn}</p>
             </Link>
@@ -12,8 +13,10 @@ export default props =>
                 <p style={{padding: `0`}}>{props.linkContact}</p>
             </Link>
         </div>
-        <div style={{display: `flex`, flexDirection: `column`, alignItems: `flex-end`, justifyContent: `center`, backgroundColor: `#072B5E`, marginTop: `0rem`, textAlign: `center` }}>
+
+        <div className="footer-text" style={{display: `flex`, flexDirection: `column`, flexWrap: `wrap`, alignItems: `flex-end`, backgroundColor: `#072B5E`, marginTop: `0rem` }}>
             <p style={{color: `#EDE90C`, fontSize: `1.3rem`}}>{props.footerText}</p>
             <p style={{margin: `0`, padding: `0`, color: `#EDE90C`}}>{props.footerQtr} {props.footerYr}</p>
         </div>
+
     </div>
