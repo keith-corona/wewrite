6ed1f2521f7f80216bc1093f68d871a0157d2b66
Keith Corona <keithcorona23@gmail.com>
2020-08-08 21:52:54 -0700

    return page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -9,20 +9,23 @@ const Contact = () => (
 <Header headerText="Stories from the Sources" link1Text= "Contact"/>
 <h1 id="title">Leave Us a Note</h1>
     <Container>
-    <form name="contact" action="/" method="POST" netlify-honeypot="bot-field" data-netlify="true">
-  <p class="hidden">
-    <label>Don’t fill this out if you're human: <input name="bot-field" /></label>
-  </p>
-  <p>
-    <label>Email: <input type="text" name="email" /></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
+    <form name="contact" method="POST" netlify-honeypot="bot-field" data-netlify="true">
+        <p class="hidden">
+            <label>Don’t fill this out if you're human: <input name="bot-field" /></label>
+        </p>
+
+        <p>
+            <label>Email: <input type="text" name="email" /></label>
+        </p>
+
+        <p>
+            <label>Message: <textarea name="message"></textarea></label>
+        </p>
+
+        <p>
+            <button type="submit">Send</button>
+        </p>
+    </form>
     </Container>
 <Footer footerReturn="Home" linkContact="Contact" footerText="This is a UCSB Linguistics 12 project" footerQtr="Spring" footerYr="2020"/>
 </div>
